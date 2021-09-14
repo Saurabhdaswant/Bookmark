63732585889920c85d4442ef554805e9185ff062
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-09-14 18:34:55 +0530

    add core components of the layout and its styling files with scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./Sass/App.scss"
+import Home from "./layout/Home"
+import Extension from "./layout/Extension"
+import Faq from "./layout/FAQ"
+import  Features from "./layout/Features"
+import Footer from "./layout/Footer"
 
 function App() {
   return (
     <div className="App">
-      helu
+      <Home/>
+      <Extension/>
+      <Faq/>
+      <Features/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/layout/Extension.js
@@ -0,0 +1,9 @@
+function Extension() {
+    return (
+        <div>
+            Extension
+        </div>
+    )
+}
+
+export default Extension
