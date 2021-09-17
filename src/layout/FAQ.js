3b9a72cf37134e629850ac50e9359bad5d9da21b
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-09-17 16:28:08 +0530

    new FAQ in the layout bruhh

--- src/layout/FAQ.js
+++ src/layout/FAQ.js
@@ -1,7 +1,11 @@
 function FAQ() {
     return (
-        <div>
-            FAQ
+        <div className="Faq">
+            <div className="Faq__head">
+                <h1>Frequently Asked Questions</h1>
+                <p>Here are some of our FAQs. if you have any other questions you</p>
+            </div>
+            <div className="Faq__main"></div>
         </div>
     )
 }
