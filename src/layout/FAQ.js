69e457b63baeaa2c4ee4804fa832d1258e09ca95
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-09-24 15:59:46 +0530

    added the Accordion Funcitonality and the button showMore

--- src/layout/FAQ.js
+++ src/layout/FAQ.js
@@ -1,9 +1,13 @@
+import { useState } from "react"
 import "../Sass/App.scss";
 import "../Sass/FAQ.scss"
 import data from "../data/Faq"
 import FaqCard from "./components/FaqCard";
 
 function FAQ() {
+
+    const [show, setShow] = useState(false);
+
     return (
         <div className="Faq">
             <div className="Faq__head">
@@ -12,8 +16,15 @@ function FAQ() {
             </div>
             <div className="Faq__main">
                 {
-                    data.map(faq => <FaqCard key={faq.id} {...faq}  />)
+                    show
+                        ? data.map(faq => <FaqCard key={faq.id} {...faq} />)
+                        : data.slice(0, data.length - 2).map(faq => <FaqCard key={faq.id} {...faq} />)
                 }
+                <div className="showMore" >
+                    <button className="btn btn--softBlue"  onClick={() => setShow(!show)}>
+                        {show ? `showLess` : `showMore`}
+                    </button>  
+                </div>
             </div>
         </div>
     )
