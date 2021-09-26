0009fb472e72208c9cc919e4c7d3a6984243af2e
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-09-26 20:17:46 +0530

    update added core functionality of the features section also in css file replaced svg to img

--- src/layout/Features.js
+++ src/layout/Features.js
@@ -1,9 +1,16 @@
 import "../Sass/App.scss";
 import "../Sass/Features.scss"
+import data from "../data/FeaturesData"
+import { useState } from "react";
 
 function Features() {
 
-    const states = [ "simple bookmarking" , "speedy searching", "easy sharing" ]
+    const [state, setstate] = useState("Simple Bookmarking")
+
+    const handleClick = (e) => {
+        const featureTitle = e.target.innerHTML
+        setstate(featureTitle);
+    }
 
     return (
         <div className="features" >
@@ -14,23 +21,46 @@ function Features() {
             <div className="features__main">
                 <div className="features__main__theFeature">
                     {
-                        states.map( theFeature => <p>{theFeature}</p>)
+                        data.map(item => <p onClick={(e) => handleClick(e)} >{item.title}</p>)
                     }
                 </div>
-                <div className="features__main__details">
-                    <div className="features__main__details__img">
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="536" height="346" viewBox="0 0 536 346" ><defs><rect id="a" width="536" height="346" rx="8"/></defs><g fill="none" fill-rule="evenodd"><mask id="b" fill="#fff"><use xlinkHref="#a"/></mask><use fill="#E8EAF6" fill-rule="nonzero" xlinkHref="#a"/><g mask="url(#b)"><path fill="#242A45" d="M-3.53.197h547v59h-547z"/><g transform="translate(22.47 14.197)" fill="#FFF"><circle cx="16" cy="16" r="16" opacity=".245"/><circle cx="477" cy="16" r="16" opacity=".104"/><g transform="translate(47 6)"><rect width="120" height="6" opacity=".75" rx="3"/><rect width="64" height="6" y="14" opacity=".25" rx="3"/></g></g></g><g mask="url(#b)"><g transform="translate(57 93)"><rect width="261" height="316" fill="#FFF" rx="8"/><rect width="128" height="316" x="295" fill="#FFF" rx="8"/><rect width="120" height="6" x="78.47" y="38.197" fill="#242A45" opacity=".5" rx="3"/></g></g><g transform="translate(88.47 115.197)"><circle cx="16" cy="26" r="16" fill="#F98E5D"/><rect width="88" height="6" x="284" fill="#242A45" opacity=".25" rx="3"/><rect width="88" height="6" x="284" y="20" fill="#242A45" opacity=".25" rx="3"/><rect width="88" height="6" x="284" y="40" fill="#242A45" opacity=".25" rx="3"/><rect width="88" height="6" x="284" y="60" fill="#242A45" opacity=".25" rx="3"/><rect width="88" height="6" x="284" y="94" fill="#242A45" opacity=".25" rx="3"/><rect width="88" height="6" x="284" y="114" fill="#242A45" opacity=".25" rx="3"/><rect width="88" height="6" x="284" y="134" fill="#242A45" opacity=".25" rx="3"/><rect width="64" height="6" x="47" y="30" fill="#242A45" opacity=".25" rx="3"/></g><g transform="translate(88.47 181.197)"><circle cx="16" cy="16" r="16" fill="#6AC6D6"/><rect width="120" height="6" x="47" y="6" fill="#242A45" opacity=".5" rx="3"/><rect width="64" height="6" x="47" y="20" fill="#242A45" opacity=".25" rx="3"/></g><g transform="translate(88.47 237.197)"><circle cx="16" cy="16" r="16" fill="#A47ED4"/><rect width="120" height="6" x="47" y="6" fill="#242A45" opacity=".5" rx="3"/><rect width="64" height="6" x="47" y="20" fill="#242A45" opacity=".25" rx="3"/></g><g transform="translate(88.47 293.197)"><circle cx="16" cy="16" r="16" fill="#EC7878"/><rect width="120" height="6" x="47" y="6" fill="#242A45" opacity=".5" rx="3"/><rect width="64" height="6" x="47" y="20" fill="#242A45" opacity=".25" rx="3"/></g></g></svg>
-                        <div className="rounded rounded--right"></div>
-                    </div>
-                    <div className="features__main__details__about">
-                        <h1 >Bookmark in one click</h1>
-                        <p>Organize your bookmarks however you like Our simple drag-and-drop interface gives you complete control over how you manage your favourited sites.</p>
-                        <button className="btn btn--softBlue" >Show More</button>
-                    </div>
-                </div>
+                {data.filter(feature => feature.title === state)
+                    .map(({ id, info, img, title }) => <div key={id} className="features__main__details">
+                        <div className="features__main__details__img">
+                        <img src={img} alt={title} />
+                            <div className="rounded rounded--right"></div>
+                        </div>
+                        <div className="features__main__details__about">
+                            <h1>{title}</h1>
+                            <p>{info}</p>
+                            <button className="btn btn--softBlue" >Show More</button>
+                        </div>
+                    </div>)}
             </div>
         </div>
     )
 }
 
 export default Features
+
+
+//!     Understand the Problem 💯
+//      a filter fucntionality which you created for menu list 
+//      onclick it should change the state and when state changes 
+//      it should change the content
+
+// todo Divide and Conquer 🏆
+//   1. Get all data together like : feature title , img , info
+//   2. Map through it 
+//   3. Conditional rendering
+//   4. Alert component
+
+//*     Research as much as you can 🔍
+//   1. JS Validation For Empty Fields : https://www.w3schools.com/howto/howto_js_validation_empty_input.asp
+//   2. How to validate an email in ReactJS ? : https://www.geeksforgeeks.org/how-to-validate-an-email-in-reactjs/
+
+//?     Write Pseudo code ✅
+//      first handle submission of the form
+//      control input using state hooks
+//      if state value is empty after submiting form then render the Alert Component
+//      Alert C : z-index : -10 , Change Absolute Position , Animation : 0.5s
