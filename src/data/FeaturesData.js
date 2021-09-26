8b74306a5b2ba253a4a734aa17219f1318988f7a
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-09-26 20:01:49 +0530

    new data for features section

---
+++ src/data/FeaturesData.js
@@ -0,0 +1,22 @@
+const Feature = [
+    {
+      id: 1,
+      title: "Simple Bookmarking",
+      img: "./svg/Simple Bookmarking.svg",
+      info: "Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.",
+    },
+    {
+        id: 2,
+        title: "Speedy Searching",
+        img: "./svg/Speedy Searching.svg",
+        info: "Organize your bookmarks however you like Our simple drag-and-drop interface gives you complete control over how you manage your favourited sites.",
+      },
+      {
+        id: 3,
+        title: "Easy Sharing",
+        img: "./svg/Easy Sharing.svg",
+        info: "We've got more browsers in the pipeline. Please do let us know if you've got a favourite you'd like us to priotize.",
+      },
+  ];
+  export default Feature;
+  
