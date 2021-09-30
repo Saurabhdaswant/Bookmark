eedf64634407611e6710fbcba3fdbff2e4e77622
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-09-30 18:26:14 +0530

    fix

--- src/layout/FooterLinks.js
+++ src/layout/FooterLinks.js
@@ -5,7 +5,7 @@ function FooterLinks() {
   return (
     <div className="Footer__Links">
       <div className="nav__icons">
-        <p>coded by <a href="https://twitter.com/DaswantSaurabh">me! </a> challenge by <a href="https://www.frontendmentor.io/challenges">FEM 🎯</a> using <img src="./svg/react-2.svg"x alt="react-js" /> <img src="./svg/sass-1.svg" alt="sass" /> </p>
+        <p>coded by <a href="https://twitter.com/DaswantSaurabh">me! </a> challenge by <a href="https://www.frontendmentor.io/challenges">FEM 🎯</a> built using <img src="./svg/react-2.svg"x alt="react-js" /> <img src="./svg/sass-1.svg" alt="sass" /> </p>
       </div>
     </div>
   );
